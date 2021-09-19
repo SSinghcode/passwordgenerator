6bcd68a6d91f79d79d9cf8cd867f455069052959
navjot turna <shamsherturna101@gmail.com>
2021-09-19 17:29:51 -0400

    intialization

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
+let generateBtn = document.querySelector("#generate");
+let smallCase = 'abcdefghijklmnopqrstuvwxyz';
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let numericCase = '0123456789';
+let specialCharacterCase = '@#$%^&*()_+~}{[]=';
+promptResponseOptionMessage = 'Enter Yes or No. Anything else will be treated as No';
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
