16ad6054f3022a995baf9b7d7ba9d6a775dfcda8
navjot turna <shamsherturna101@gmail.com>
2021-09-19 17:51:29 -0400

    password status

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ let numericCase = '0123456789';
 let specialCharacterCase = '@#$%^&*()_+~}{[]=';
 promptResponseOptionMessage = 'Enter Yes or No. Anything else will be treated as No';
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  if (password) {
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  } else {
+    alert('Sorry ! Password can not be generated.Please try again')
+  }
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+let finalChar = '';
+
+let lengthOfPassword = promptLength();
+
+if (!lengthOfPassword) {
+  alert('Enter a valid length.Please try again');
+  return '';
+}
